c9459bf4ef884aa2f3a89eb5048f5f146773cddc
Lucas Ament <lucasament5@gmail.com>
2021-10-21 20:42:14 -0300

    Add function closeConection

--- class/BD.js
+++ class/BD.js
@@ -1,21 +1,23 @@
 var mysql      = require('mysql');
-const dataBaseConnect = () =>{
-  var connection = mysql.createConnection({
-    host     : '162.241.2.146',
-    user     : 'anunci87_feedbuy',
-    password : '1.~;nMTj{AW]a;f(JN'
-  });
+  const dataBaseConnect = () =>{
+    var connection = mysql.createConnection({
+      host     : '162.241.2.146',
+      user     : 'anunci87_feedbuy',
+      password : '1.~;nMTj{AW]a;f(JN'
+    });
 
-  connection.connect();
-
-  connection.end();
-}
+    connection.connect();
+  }
 
 
-const executeQuery = async(query) => {
-  connection.query(query, function(err, rows, fields) {
-    if (err) throw err;
-    console.log('The solution is: ', rows[0].solution);
-    Promise.resolve(rows);
-  });
+  const executeQuery = async(query) => {
+    connection.query(query, function(err, rows, fields) {
+      if (err) throw err;
+      console.log('The solution is: ', rows[0].solution);
+      Promise.resolve(rows);
+    });
+
+  const closeConection = () => {
+    connection.end();
+  }
 }
